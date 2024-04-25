76f9df48833d9ac66cba086f4f9b7cb5837b7f05
ToshaETang <a0320506@gmail.com>
2024-04-25 20:02:24 +0800

    架構重整

--- clhs-doos-front/src/App.js
+++ clhs-doos-front/src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class App extends Component {
     render() {
         return (
-            <h1>This is App component</h1>
+            <h1>CLHS-Delicatessen-Online-Order-System</h1>
         );
     }
 }
